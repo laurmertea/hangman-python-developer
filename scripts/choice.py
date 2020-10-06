7982b2003a93486b0d68d0b13055f440ac0d9454
laurmertea <mertealaur@yahoo.com>
2020-10-06 23:30:25 +0200

    Add third stage script description, objectives & examples

---
+++ scripts/choice.py
@@ -0,0 +1,26 @@
+# Description
+# If there is a predefined word, the game isn't replayable. 
+# You already know the word, so there’s no longer any challenge. 
+# At this stage, let's make the game more challenging by choosing a word from a special list with a variety of options. 
+# This way, our game will have more replay value.
+#
+# Objectives
+# Create the following list of words: 'python', 'java', 'kotlin', 'javascript'.
+# Program the game to choose a word from the list at random. You can enter more words, but let's stick to these four for now.
+#
+# Examples
+# The greater-than symbol followed by space (> ) represents the user input. Note that it's not part of the output.
+# Example 1, The computer randomly chose python from the list.
+# H A N G M A N
+# Guess the word: > python
+# You survived!
+#
+# Example 2, the computer randomly chose something other than python from the list.
+# H A N G M A N
+# Guess the word: > python
+# You lost!
+#
+# Example 3, the computer randomly chose something other than kotlin from the list.
+# H A N G M A N
+# Guess the word: > kotlin
+# You lost!
