3b9ca184a690c0ca76e76ceb4c9b902192afa636
laurmertea <mertealaur@yahoo.com>
2020-10-06 23:50:01 +0200

    Add fourth stage script description, objectives & examples

---
+++ scripts/hint.py
@@ -0,0 +1,20 @@
+# Description
+# Now our game has become quite hard, and your chances of guessing the word depend on the size of the list. 
+# When the list has four words, you only have a 25% chance to guess correctly. 
+# So let's show a little mercy to the player and add a hint for them.
+#
+# Objectives
+# As in the previous stage, you should use the following word list: 'python', 'java', 'kotlin', 'javascript'
+# Once the computer has chosen a word from the list, show its first 3 letters. Hidden letters should be replaced with hyphens ("-").
+#
+# Examples
+# The greater-than symbol followed by space (> ) represents the user input. Note that it's not part of the output.
+# Example 1
+# H A N G M A N
+# Guess the word jav-: > java
+# You survived!
+#
+# Example 2
+# H A N G M A N
+# Guess the word pyt---: > pythia
+# You lost!
